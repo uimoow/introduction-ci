88f65bc6ff2a116b349c37ad79535810b339e863
uimoow <min32ryk@gmail.com>
2023-09-16 20:40:46 +0900

    登録・更新機能を追加

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,35 +1,54 @@
+import { ProductService } from './product.service';
+import { CreateProductDTO } from './dto/create-product.dto';
+import { Product } from './product.entity';
+import { DeleteResult, InsertResult } from 'typeorm';
 import {
-    Controller,
-    Post,
-    Body,
-    Get,
-    Patch,
-    Param,
-    Delete,
-  } from '@nestjs/common';
-  import { ProductService } from './product.service';
-  import { CreateProductDTO } from './dto/create-product.dto';
-  import { Product } from './product.entity';
-  
-  @Controller('product')
-  export class ProductController {
-    constructor(private productService: ProductService) {}
-  
-    @Get('all')
-    public async getProducts(): Promise<Product[]> {
-      const products = await this.productService.getProducts();
-      return products;
-    }
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 
-    @Get('/:productId')
-    public async getProduct(@Param('productId') productId: number) {
-      const product = await this.productService.getProduct(productId);
-      return product;
-    }
+@Controller('product')
+export class ProductController {
+  constructor(private productService: ProductService) { }
 
-    @Delete('/delete/:productId')
-    public async deleteProduct(@Param('productId') productId: number) {
-      const deletedProduct = await this.productService.deleteProduct(productId);
-      return deletedProduct;
-    }
+  @Post('create')
+  public async createProduct(
+    @Body() createProductDto: CreateProductDTO,
+  ): Promise<Product> {
+    return await this.productService.createProduct(createProductDto);
+  }
+
+  @Get('all')
+  public async getProducts(): Promise<Product[]> {
+    return await this.productService.getProducts();
+  }
+
+  @Get('/:productId')
+  public async getProduct(@Param('productId') productId: number
+  ): Promise<Product> {
+    return await this.productService.getProduct(productId);
+  }
+
+  @Patch('/update/:productId')
+  public async editProduct(
+    @Body() updateProductDto: CreateProductDTO,
+    @Param('productId') productId: number,
+  ): Promise<Product> {
+    const product = await this.productService.updateProduct(
+      productId,
+      updateProductDto,
+    );
+    return product;
+  }
+
+  @Delete('/delete/:productId')
+  public async deleteProduct(@Param('productId') productId: number
+  ): Promise<DeleteResult> {
+    return this.productService.deleteProduct(productId);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,28 +1,53 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Product } from './product.entity';
-import { Repository } from 'typeorm';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { DeleteResult, InsertResult, Repository } from 'typeorm';
+import { Product } from './product.entity';
+import { CreateProductDTO } from './dto/create-product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(
     @InjectRepository(Product)
-    private productRepository: Repository<Product>,
+    private readonly productRepository: Repository<Product>,
   ) { }
+
+  public async createProduct(
+    createProductDto: CreateProductDTO,
+  ): Promise<Product> {
+    const result: InsertResult = await this.productRepository.insert(createProductDto)
+      .catch((e) => {
+        throw new InternalServerErrorException(`[${e.message}]: Failed to create product`,);
+      });
+    return this.getProduct(result.identifiers[0].id);
+  }
 
   public async getProducts(): Promise<Product[]> {
     return await this.productRepository.find();
   }
 
   public async getProduct(productId: number): Promise<Product> {
-    const foundProduct = await this.productRepository.findOneBy({ id: productId });
+    const foundProduct: Product = await this.productRepository.findOneBy({ id: productId });
     if (!foundProduct) {
       throw new NotFoundException('Product not found');
     }
     return foundProduct;
   }
 
-  public async deleteProduct(productId: number): Promise<void> {
-    await this.productRepository.delete(productId);
+  public async updateProduct(
+    productId: number,
+    updateProductDto: CreateProductDTO,
+  ): Promise<Product> {
+    const targetProduct = await this.productRepository.findOneBy({ id: productId });
+    if (!targetProduct) {
+      throw new NotFoundException('Product not found');
+    }
+    return this.productRepository.save(updateProductDto);
+  }
+
+  public async deleteProduct(productId: number): Promise<DeleteResult> {
+    return await this.productRepository.delete(productId)
+      .catch((e) => {
+        throw new InternalServerErrorException(`[${e.message}]: Failed to delete product`,);
+      });
   }
 }
